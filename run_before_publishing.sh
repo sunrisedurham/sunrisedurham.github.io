40a06b05883aff5559bab237fdec03c4bbac61e3
BenBean18 <beakerthespeaker@gmail.com>
2021-06-12 13:50:24 -0400

    Modified the Local Politics page

--- run_before_publishing.sh
+++ run_before_publishing.sh
@@ -1,7 +1,15 @@
-hash=$(git hash-object resources/current_alert.js)
+currentAlertHash=$(git hash-object resources/current_alert.js)
+navbarHash=$(git hash-object resources/navbar.js)
+
 rm resources/current_alert.*.js
-cp resources/current_alert.js "resources/current_alert.$hash.js"
-sed -Ei'.backup' 's/current_alert.*js/current_alert.'"$hash"'.js/g' *.html
-sed -Ei'.backup' 's/current_alert.*js/current_alert.'"$hash"'.js/g' campaigns/*.html
+cp resources/current_alert.js "resources/current_alert.$currentAlertHash.js"
+sed -Ei'.backup' 's/current_alert.*js/current_alert.'"$currentAlertHash"'.js/g' *.html
+sed -Ei'.backup' 's/current_alert.*js/current_alert.'"$currentAlertHash"'.js/g' campaigns/*.html
+
+rm resources/navbar.*.js
+cp resources/navbar.js "resources/navbar.$navbarHash.js"
+sed -Ei'.backup' 's/navbar.*js/navbar.'"$navbarHash"'.js/g' *.html
+sed -Ei'.backup' 's/navbar.*js/navbar.'"$navbarHash"'.js/g' campaigns/*.html
+
 rm *.backup
 rm campaigns/*.backup
