294eedbfbb916f688dc8efd85e399749e16b514c
Felicia Wang <fxw3@duke.edu>
2023-02-06 11:07:08 -0500

    separate action pages

--- resources/navbar.js
+++ resources/navbar.js
@@ -13,22 +13,15 @@ let navbar = `<nav class="navbar navbar-expand-lg bg-sunrise-dark-overlay">
                     Home
                 </a>
             </li>
-            <li class="nav-item dropdown">
-                <button class="btn btn-outline-sunrise-red m-1" id="campaignDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                    Campaigns
-                </button>
-                <ul class="dropdown-menu outline-sunrise-yellow bg-sunrise-dark" aria-labelledby="campaignDropdown">
-                    <li>
-                        <a class="dropdown-item-sunrise-red" href="campaigns/good_jobs_for_all.html">Good Jobs For All</a>
-                        <a class="dropdown-item-sunrise-red" href="campaigns/duke_energy.html">Duke Energy</a>
-                        <a class="dropdown-item-sunrise-red" href="campaigns/local_politics.html">Local Politics</a>
-                        <a class="dropdown-item-sunrise-red" href="campaigns/housing_climate_justice.html">Housing & Climate Justice</a>
-                    </li>
-                </ul>
-            </li>
+            
             <li class="nav-item">
                 <a class="btn btn-outline-sunrise-red m-1" href="actions.html" id="actions">
                     Recent Actions
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="btn btn-outline-sunrise-red m-1" href="upcoming-actions.html" id="upcoming-actions">
+                    Upcoming Actions
                 </a>
             </li>
             <li class="nav-item">
@@ -43,3 +36,17 @@ let navbar = `<nav class="navbar navbar-expand-lg bg-sunrise-dark-overlay">
 let page = document.getElementById("navbar").getAttribute("data-page")
 document.getElementById("navbar").outerHTML = navbar
 document.getElementById(page).classList.add("active")
+
+/*<li class="nav-item dropdown">
+                <button class="btn btn-outline-sunrise-red m-1" id="campaignDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                    Campaigns
+                </button>
+                <ul class="dropdown-menu outline-sunrise-yellow bg-sunrise-dark" aria-labelledby="campaignDropdown">
+                    <li>
+                        <a class="dropdown-item-sunrise-red" href="campaigns/good_jobs_for_all.html">Good Jobs For All</a>
+                        <a class="dropdown-item-sunrise-red" href="campaigns/duke_energy.html">Duke Energy</a>
+                        <a class="dropdown-item-sunrise-red" href="campaigns/local_politics.html">Local Politics</a>
+                        <a class="dropdown-item-sunrise-red" href="campaigns/housing_climate_justice.html">Housing & Climate Justice</a>
+                    </li>
+                </ul>
+            </li>*/
